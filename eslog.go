bd571d31436edd01ecd9123e99b10a33e67805d3
YuFeng <feng950704@foxmail.com>
2018-04-12 15:23:03 +0800

    Initial commit

---
+++ eslog.go
@@ -0,0 +1,25 @@
+package loglib
+
+import "fmt"
+
+type ESLogData struct {
+	*BaseLogData
+	Msg string
+}
+
+//记录ES错误日志
+func (self *MLog) Printf(format string, v ...interface{}) {
+	baseLogData := &BaseLogData{
+		Tip:    "ElasticSearch",
+		Source: getCallerFile(),
+		Tag:    "ES",
+		Level:  LevelError,
+	}
+
+	esLogData := &ESLogData{
+		Msg: fmt.Sprintf(format, v...),
+	}
+	esLogData.BaseLogData = baseLogData
+	self.writeMsg(esLogData)
+}
+
